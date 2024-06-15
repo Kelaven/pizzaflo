d8b9cd5ab5929fc3f1feca4ebaccc0df65606d31
Kelaven <kevin.lavenant@live.fr>
2024-06-15 17:44:28 +0200

    feat(fix optimisation): lighthouse improved

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,21 @@
 import { defineConfig } from "vite";
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'robots.txt',
+          dest: '.'
+        },
+        // {
+        //   src: 'node_modules/preline/dist/preline.js',
+        //   dest: 'assets/js' // Vous pouvez choisir le répertoire de destination souhaité
+        // }
+      ]
+    })
+  ],
   root: "src",
   build: {
     outDir: "../dist", // Où Vite construira les fichiers de production
@@ -9,6 +24,9 @@ export default defineConfig({
         main: "src/index.html",
       },
     },
+  },
+  optimizeDeps: {
+    include: ['preline']
   },
   publicDir: "../public", // Répertoire des fichiers statiques
 });
