254b9ab1202f0e79bdd7a9535843f9df804b92cf
Kelaven <kevin.lavenant@live.fr>
2024-06-27 17:52:54 +0200

    feat(contact section): front created

--- src/app/app.js
+++ src/app/app.js
@@ -1,10 +1,11 @@
 import "../styles/index.css";
 import { smoothScale } from "./features/expertise.js";
 import { animateCardsModal } from "./features/customers.js";
+import { eventDatePlaceholder } from "./features/contact.js";
 
 smoothScale();
 animateCardsModal();
-
+eventDatePlaceholder();
 
 
 
