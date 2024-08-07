bcbfc782c53af9734c23b8ed71b365f6627d31da
Kelaven <kevin.lavenant@live.fr>
2024-08-07 17:41:16 +0200

    fix(general): minor corrections

--- src/app/style.js
+++ src/app/style.js
@@ -9,7 +9,7 @@ document.body.addEventListener('contextmenu', (event) => { // au click droit
     if (event.target.tagName === 'IMG') { // si l'élément ciblé est une image. Procéder comme ça plutôt qu'en sélectionnant toutes les images avec un querySelectorAll permet de cibler aussi les images générées dynamiquement (donc celles de la carte des pizzas)
         // Créer le tooltip
         const tooltip = document.createElement('div');
-        tooltip.className = 'absolute bg-black text-white text-sm rounded-md p-2 shadow-md z-100';
+        tooltip.className = 'absolute bg-black text-white text-sm rounded-md p-2 shadow-md z-50';
         tooltip.innerText = 'Images protégées © Pizza Flo';
 
         // Ajouter le tooltip au document
