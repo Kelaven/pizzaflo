0b7a1d206ce8f12c8e025ba325fc73f9b700621e
Kelaven <kevin.lavenant@live.fr>
2024-08-20 12:43:38 +0200

    fix(general): date into the form fixed and tel added in navbar

--- src/app/app.js
+++ src/app/app.js
@@ -1,11 +1,11 @@
 import { smoothScale } from "./features/homepage/expertise.js";
 import { animateCardsModal } from "./features/homepage/customers.js";
-import { eventDatePlaceholder } from "./features/homepage/contact.js";
+// import { eventDatePlaceholder } from "./features/homepage/contact.js";
 import { formAPI } from "./features/homepage/form.js";
 
 smoothScale();
 animateCardsModal();
-eventDatePlaceholder();
+// eventDatePlaceholder();
 formAPI();
 
 
--- src/app/features/homepage/contact.js
+++ src/app/features/homepage/contact.js
@@ -1,53 +1,53 @@
-// Variable
-const dateInput = document.querySelector("#date");
+// // Variable
+// const dateInput = document.querySelector("#date");
 
-// Fonctions
-const changeTypeToDate = () => {
-    dateInput.type = 'date';
-    dateInput.placeholder = ''; // Supprimer le placeholder
-    // console.log('Type changé en date:', dateInput, dateInput.value);
-}
-const changeTypeToText = () => {
-    dateInput.type = 'text';
-    dateInput.placeholder = '01/01/2024'; // Supprimer le placeholder
-    // console.log('Type changé en text:', dateInput);
-}
+// // Fonctions
+// const changeTypeToDate = () => {
+//     dateInput.type = 'date';
+//     dateInput.placeholder = ''; // Supprimer le placeholder
+//     // console.log('Type changé en date:', dateInput, dateInput.value);
+// }
+// const changeTypeToText = () => {
+//     dateInput.type = 'text';
+//     dateInput.placeholder = '01/01/2024'; // Supprimer le placeholder
+//     // console.log('Type changé en text:', dateInput);
+// }
 
-// Evenement
-export const eventDatePlaceholder = () => {
+// // Evenement
+// export const eventDatePlaceholder = () => {
 
-    dateInput.addEventListener("focus", changeTypeToDate); // focus fonctionne quand on entre dans l'input
+//     dateInput.addEventListener("focus", changeTypeToDate); // focus fonctionne quand on entre dans l'input
 
-    dateInput.addEventListener("change", () => { // change fonctionne quand on change la valeur
-        if (dateInput.value) {
-            changeTypeToDate();
-        } else {
-            changeTypeToText();
-        }
-    });
+//     dateInput.addEventListener("change", () => { // change fonctionne quand on change la valeur
+//         if (dateInput.value) {
+//             changeTypeToDate();
+//         } else {
+//             changeTypeToText();
+//         }
+//     });
 
-    dateInput.addEventListener("blur", () => { // change fonctionne quand on perd le focus
+//     dateInput.addEventListener("blur", () => { // change fonctionne quand on perd le focus
 
-        if (dateInput.value === '') {
-            changeTypeToText();
-        } else {
-            changeTypeToDate();
-        }
-    });
+//         if (dateInput.value === '') {
+//             changeTypeToText();
+//         } else {
+//             changeTypeToDate();
+//         }
+//     });
 
-    // Vérification front à la validation du formulaire
-    const form = document.querySelector("form");
-    form.addEventListener("submit", (e) => {
-        if (dateInput.type === 'text' && dateInput.value !== "") { // && ne pas avoir l'alert quand on laisse l'input vide car la date n'est pas obligatoire
-            e.preventDefault();
-            alert("Veuillez entrer une date valide.");
-        }
-    });
-    // console.log(dateInput);
-};
+//     // Vérification front à la validation du formulaire
+//     const form = document.querySelector("form");
+//     form.addEventListener("submit", (e) => {
+//         if (dateInput.type === 'text' && dateInput.value !== "") { // && ne pas avoir l'alert quand on laisse l'input vide car la date n'est pas obligatoire
+//             e.preventDefault();
+//             alert("Veuillez entrer une date valide.");
+//         }
+//     });
+//     // console.log(dateInput);
+// };
 
-// Comments
-    // Lorsqu’un utilisateur focalise le champ, il devient un champ de type date et le placeholder est supprimé.
-    // Lorsque la valeur du champ change, le type reste date si une valeur est présente, sinon il repasse en text avec le placeholder.
-    // Lorsque l’utilisateur perd le focus et que le champ est vide, il redevient un champ de type text avec le placeholder initial, sinon il reste un champ de type date.
-    // Avant de soumettre le formulaire, le code vérifie si le champ de date est toujours de type text. Si c’est le cas, le formulaire ne sera pas soumis et un message d’alerte s’affichera.
+// // Comments
+//     // Lorsqu’un utilisateur focalise le champ, il devient un champ de type date et le placeholder est supprimé.
+//     // Lorsque la valeur du champ change, le type reste date si une valeur est présente, sinon il repasse en text avec le placeholder.
+//     // Lorsque l’utilisateur perd le focus et que le champ est vide, il redevient un champ de type text avec le placeholder initial, sinon il reste un champ de type date.
+//     // Avant de soumettre le formulaire, le code vérifie si le champ de date est toujours de type text. Si c’est le cas, le formulaire ne sera pas soumis et un message d’alerte s’affichera.
