0ee262226f63717ed1d66b7f2ea89e1f53ce8bd0
Kelaven <kevin.lavenant@live.fr>
2024-08-22 11:20:54 +0200

    fix(general): CORS bugs fixed

--- src/indexForm.php
+++ src/indexForm.php
@@ -1,5 +1,8 @@
 <?php
 header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: https://www.test.kevin-lavenant.fr");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE, PUT");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 require_once __DIR__ . '/config/regex.php';
 
