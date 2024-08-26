608fd5502fc57db3c77cc2b1e7c9d76895f15d2c
Kelaven <kevin.lavenant@live.fr>
2024-08-26 15:37:13 +0200

    fix(general): site deployed

--- src/indexForm.php
+++ src/indexForm.php
@@ -1,6 +1,7 @@
 <?php
 header('Content-Type: application/json');
-header("Access-Control-Allow-Origin: https://test.kevin-lavenant.fr");
+// header("Access-Control-Allow-Origin: https://test.kevin-lavenant.fr");
+header("Access-Control-Allow-Origin: https://pizza-flo.fr/");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE, PUT");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
